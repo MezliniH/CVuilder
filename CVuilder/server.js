2d3a7e14852a23ad84416a7ec0deb45d190f8ca4
MezliniH <die.mezlini@gmail.com>
2022-03-11 09:08:59 +0100

    CV builder project 0.1.0

---
+++ CVuilder/server.js
@@ -0,0 +1,37 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+const pdf = require("html-pdf");
+const cors = require("cors");
+
+const app = express();
+
+const pdfTemplate = require("./documents");
+
+const options = {
+	height: "42cm",
+	width: "29.7cm",
+	timeout: "600",
+};
+
+app.use(cors());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+
+// POST route for PDF generation....
+app.post("/create-pdf", (req, res) => {
+	pdf.create(pdfTemplate(req.body), options).toFile("Resume.pdf", (err) => {
+		if (err) {
+			console.log(err);
+			res.send(Promise.reject());
+		} else res.send(Promise.resolve());
+	});
+});
+
+// GET route -> send generated PDF to client...
+app.get("/fetch-pdf", (req, res) => {
+	const file = `${__dirname}/Resume.pdf`;
+	res.download(file);
+});
+
+const port = process.env.PORT || 5000;
+app.listen(port, () => console.log(`Server started on port ${port}`));
